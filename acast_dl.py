c8be31057afaa08e8414c990f050070b52bced5e
FrankSansC <frank@sans-c.fr>
2025-08-04 22:27:39 +0200

    Add PEP 723 metadata to handle external dependencies using `uv`.
    Signed-off-by: FrankSansC <frank@sans-c.fr>

--- acast_dl.py
+++ acast_dl.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#     "feedparser",
+#     "mutagen"
+# ]
+# ///
+
 import os
 import re
 import feedparser
